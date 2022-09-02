722131265ba0df57364cc70e87ec05207df0e9e6
hays0503 <cthtufgbv@mail.ru>
2022-09-02 17:13:04 +0600

    Revert "Revert "Разбивка на микросервисы Orders""
    This reverts commit 265538464d264c7966773756ca16b0345b163e07.

--- src/components/DocumentControl/pages/OnApprovalDocuments/forms/4/Update4.js
+++ src/components/DocumentControl/pages/OnApprovalDocuments/forms/4/Update4.js
@@ -14,7 +14,7 @@ import TaskModalUpdate from "../../modals/TaskModalUpdate";
 import UpdateTask4 from "./UpdateTask4";
 import { FormWrap, FormItem } from "./../../../fragments/FragmentItemWrap";
 import FragmentUploader from "./../../../fragments/FragmentUploader";
-import FragmentStepViewer from "../../../fragments/FragmentStepViewer";
+import { FragmentStepViewer } from "../../../fragments/FragmentStepViewer";
 import { FragmentButtons } from "./../../../fragments/FragmentButtons";
 import { FragmentReasonsViewer } from "../../../fragments/FragmentReasonsViewer";
 import { FragmentTaskList } from "../../../fragments/FragmentTaskList";
