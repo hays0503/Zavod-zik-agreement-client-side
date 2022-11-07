b095a7aebee686ef7fe8c700c783c9e4cb4684a4
RyndychRD <r.roman1234@yandex.ru>
2022-11-07 11:28:44 +0600

    Переформатирование после сохранения

--- src/components/DocumentControl/pages/fragments/FragmentSelectItems.js
+++ src/components/DocumentControl/pages/fragments/FragmentSelectItems.js
@@ -6,24 +6,24 @@ import { useEffect, useState } from "react";
  *	@description C помощью этого `gql` запроса будут запрашиваться свободные вакансии в каком либо департаменте
  */
 const positions = gql`
-	query get_free_position($positions: JSON) {
-		get_free_position(positions: $positions) {
-			id
-			name
-			id_depart
-		}
-	}
+  query get_free_position($positions: JSON) {
+    get_free_position(positions: $positions) {
+      id
+      name
+      id_depart
+    }
+  }
 `;
 /**
  * @description С помощью этого `gql` запроса будут запрашиваться словари с названием департаментов
  */
 const DepartmentDictionary = gql`
-	query department_dictionary($department_dictionary: JSON) {
-		department_dictionary(department_dictionary: $department_dictionary) {
-			id
-			department_name
-		}
-	}
+  query department_dictionary($department_dictionary: JSON) {
+    department_dictionary(department_dictionary: $department_dictionary) {
+      id
+      department_name
+    }
+  }
 `;
 
 /**
@@ -34,104 +34,104 @@ const DepartmentDictionary = gql`
  * @return {Int} `Index` Возвращает индекс выбранного элемента
  */
 export const FragmentSelectItems = (props) => {
-	const [idDepartment, setIdDepartment] = useState(props?.idDepartment);
+  const [idDepartment, setIdDepartment] = useState(props?.idDepartment);
 
-	const [isClick, setClick] = useState(true);
+  const [isClick, setClick] = useState(true);
 
-	const QueryDepartment = useQuery(positions, {
-		onCompleted: (Data) => {
-			//console.log("onCompleted:(Data)", Data);
-			//console.log("onCompleted:(Data)", QueryDepartment?.data);
-		},
-		variables: {
-			positions: {
-				global: {
-					id_depart: `=${idDepartment}`,
-				},
-			},
-		},
-	});
+  const QueryDepartment = useQuery(positions, {
+    onCompleted: (Data) => {
+      //console.log("onCompleted:(Data)", Data);
+      //console.log("onCompleted:(Data)", QueryDepartment?.data);
+    },
+    variables: {
+      positions: {
+        global: {
+          id_depart: `=${idDepartment}`,
+        },
+      },
+    },
+  });
 
-	// Запрос словаря с наименованием департаментов
-	const { loading, error, data } = useQuery(DepartmentDictionary);
-	// Возникла ошибка
-	if (error) return <p>Ошибка: {error.message}</p>;
-	// Если загрузка показываем прелоадер
-	if (loading || !data)
-		return (
-			<>
-				<Spin />
-				"Загрузка FragmentSelectItems !"
-			</>
-		);
-	// Отображение списка с наименованием департаментов
+  // Запрос словаря с наименованием департаментов
+  const { loading, error, data } = useQuery(DepartmentDictionary);
+  // Возникла ошибка
+  if (error) return <p>Ошибка: {error.message}</p>;
+  // Если загрузка показываем прелоадер
+  if (loading || !data)
+    return (
+      <>
+        <Spin />
+        "Загрузка FragmentSelectItems !"
+      </>
+    );
+  // Отображение списка с наименованием департаментов
 
-	const onChange = (value) => {
-		setIdDepartment(value);
-		setClick(false);
-	};
+  const onChange = (value) => {
+    setIdDepartment(value);
+    setClick(false);
+  };
 
-	return (
-		<>
-			<h2>Наименование департаментов</h2>
-			<Select
-				showSearch
-				style={{
-					width: 200,
-				}}
-				placeholder="Search to Select"
-				optionFilterProp="children"
-				filterOption={(input, option) => option.children.includes(input)}
-				filterSort={(optionA, optionB) =>
-					optionA.children
-						.toLowerCase()
-						.localeCompare(optionB.children.toLowerCase())
-				}
-				onChange={onChange}
-				disabled={props.disabled}
-				defaultValue={
-					data?.department_dictionary[idDepartment + 1]?.department_name
-				}
-			>
-				{data.department_dictionary.map((Item) => {
-					return (
-						<Select.Option value={Item.id}>
-							{Item.department_name}
-						</Select.Option>
-					);
-				})}
-			</Select>
-			<Divider type="vertical" />
+  return (
+    <>
+      <h2>Наименование департаментов</h2>
+      <Select
+        showSearch
+        style={{
+          width: 200,
+        }}
+        placeholder="Search to Select"
+        optionFilterProp="children"
+        filterOption={(input, option) => option.children.includes(input)}
+        filterSort={(optionA, optionB) =>
+          optionA.children
+            .toLowerCase()
+            .localeCompare(optionB.children.toLowerCase())
+        }
+        onChange={onChange}
+        disabled={props.disabled}
+        defaultValue={
+          data?.department_dictionary[idDepartment + 1]?.department_name
+        }
+      >
+        {data.department_dictionary.map((Item) => {
+          return (
+            <Select.Option value={Item.id}>
+              {Item.department_name}
+            </Select.Option>
+          );
+        })}
+      </Select>
+      <Divider type="vertical" />
 
-			<Select
-				showSearch
-				style={{
-					width: 200,
-				}}
-				placeholder="Search to Select"
-				optionFilterProp="children"
-				filterOption={(input, option) => option.children.includes(input)}
-				filterSort={(optionA, optionB) =>
-					optionA.children
-						.toLowerCase()
-						.localeCompare(optionB.children.toLowerCase())
-				}
-				disabled={
-					(QueryDepartment?.data?.get_free_position !== undefined &&
-						QueryDepartment?.data?.get_free_position.length === 0) ||
-					isClick
-				}
-				onChange={(value) => {
-					props.onChange([value]);
-				}}
-				defaultValue={props.value}
-			>
-				{QueryDepartment.data?.get_free_position.map((Item) => {
-					return <Select.Option value={Item.id}>{Item.name}</Select.Option>;
-				})}
-			</Select>
-		</>
-	);
+      <Select
+        showSearch
+        style={{
+          width: 200,
+        }}
+        placeholder="Search to Select"
+        optionFilterProp="children"
+        filterOption={(input, option) => option.children.includes(input)}
+        filterSort={(optionA, optionB) =>
+          optionA.children
+            .toLowerCase()
+            .localeCompare(optionB.children.toLowerCase())
+        }
+        disabled={
+          (QueryDepartment?.data?.get_free_position !== undefined &&
+            QueryDepartment?.data?.get_free_position.length === 0) ||
+          isClick
+        }
+        onChange={(value) => {
+          props.onChange([value]);
+        }}
+        defaultValue={props.value}
+      >
+        {QueryDepartment.data?.get_free_position.map((Item) => {
+          return <Select.Option value={Item.id}>{Item.name}</Select.Option>;
+        })}
+      </Select>
+    </>
+  );
 };
 
 /**
@@ -140,58 +140,58 @@ export const FragmentSelectItems = (props) => {
  * @param  {setFunc} `setStateValue` функция по установки стейта
  */
 export const FragmentSelectDepartment = (props) => {
-	// Запрос словаря с наименованием департаментов
-	const { loading, error, data } = useQuery(DepartmentDictionary);
-	// Возникла ошибка
-	if (error) return <p>Ошибка: {error.message}</p>;
-	// Если загрузка показываем прелоадер
-	if (loading || !data)
-		return (
-			<>
-				<Spin />
-				"Загрузка FragmentSelectItems !"
-			</>
-		);
-	// Отображение списка с наименованием департаментов
+  // Запрос словаря с наименованием департаментов
+  const { loading, error, data } = useQuery(DepartmentDictionary);
+  // Возникла ошибка
+  if (error) return <p>Ошибка: {error.message}</p>;
+  // Если загрузка показываем прелоадер
+  if (loading || !data)
+    return (
+      <>
+        <Spin />
+        "Загрузка FragmentSelectItems !"
+      </>
+    );
+  // Отображение списка с наименованием департаментов
 
-	const onChange = (value) => {
-		const newState = {
-			id: props.stateValue.id,
-			name: props.stateValue.name,
-			accesses: props.stateValue.accesses,
-			log_username: props.stateValue.log_username,
-			is_boss: props.stateValue.is_boss,
-			is_vice_director: props.stateValue.is_vice_director,
-			is_user: props.stateValue.is_user,
-			id_depart: value,
-		};
-		props.setStateValue(newState);
-	};
+  const onChange = (value) => {
+    const newState = {
+      id: props.stateValue.id,
+      name: props.stateValue.name,
+      accesses: props.stateValue.accesses,
+      log_username: props.stateValue.log_username,
+      is_boss: props.stateValue.is_boss,
+      is_vice_director: props.stateValue.is_vice_director,
+      is_user: props.stateValue.is_user,
+      id_depart: value,
+    };
+    props.setStateValue(newState);
+  };
 
-	return (
-		<Select
-			showSearch
-			style={{
-				width: 400,
-			}}
-			placeholder="Search to Select"
-			optionFilterProp="children"
-			filterOption={(input, option) => option.children.includes(input)}
-			filterSort={(optionA, optionB) =>
-				optionA.children
-					.toLowerCase()
-					.localeCompare(optionB.children.toLowerCase())
-			}
-			defaultValue={
-				props?.stateValue !== undefined ? props?.stateValue.id_depart : 0
-			}
-			onChange={onChange}
-		>
-			{data.department_dictionary.map((Item) => {
-				return (
-					<Select.Option value={Item.id}>{Item.department_name}</Select.Option>
-				);
-			})}
-		</Select>
-	);
+  return (
+    <Select
+      showSearch
+      style={{
+        width: 400,
+      }}
+      placeholder="Search to Select"
+      optionFilterProp="children"
+      filterOption={(input, option) => option.children.includes(input)}
+      filterSort={(optionA, optionB) =>
+        optionA.children
+          .toLowerCase()
+          .localeCompare(optionB.children.toLowerCase())
+      }
+      defaultValue={
+        props?.stateValue !== undefined ? props?.stateValue.id_depart : 0
+      }
+      onChange={onChange}
+    >
+      {data.department_dictionary.map((Item) => {
+        return (
+          <Select.Option value={Item.id}>{Item.department_name}</Select.Option>
+        );
+      })}
+    </Select>
+  );
 };
